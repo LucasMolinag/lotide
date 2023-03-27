d972f143f8405f271c65d6c6fd533b4e10ba552b
Lucas <molinag.lucas@gmail.com>
2023-03-26 18:16:52 -0700

    without implemented

---
+++ without.js
@@ -0,0 +1,9 @@
+const without = function(source, itemsToRemove) {
+  let resultArray = []
+  for (let item of source) {
+    if (!itemsToRemove.includes(item)) {
+        resultArray.push(item);
+    }
+  }
+  return resultArray;
+}
