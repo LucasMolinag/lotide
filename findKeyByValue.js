41b360a9beb639bbb5d08252da1fcfd93685f258
Lucas <molinag.lucas@gmail.com>
2023-04-01 20:20:51 -0700

    findKeyByValue implemented

---
+++ findKeyByValue.js
@@ -0,0 +1,17 @@
+const findKeyByValue = function(theme, showName) {
+
+  for (const key in theme) {
+    if (showName === theme[key]){
+      return (key)
+    } 
+  }
+  return('No Matches')
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
