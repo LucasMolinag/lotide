f21c7857fa23d407e920bea42855f0a586cbd083
Lucas <molinag.lucas@gmail.com>
2023-04-09 20:22:12 -0700

    takeUntil implemented

---
+++ takeUntil.js
@@ -0,0 +1,11 @@
+const takeUntil = function(array, callback) {
+  const result = [];
+ for (let value of array) {
+  if (!callback(value)) {
+    result.push(value)
+  } else {
+    break;
+  }
+ }
+ return result;
+}
