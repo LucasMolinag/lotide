a57daf27e33304d34bab145cd6a125583cf6a50c
Lucas <molinag.lucas@gmail.com>
2023-04-17 13:31:54 -0700

    Add README and middle

---
+++ middle.js
@@ -0,0 +1,13 @@
+const middle = function(array) {
+  const middleIndex = Math.floor(array.length / 2);
+
+  if(array.length === 1 || array.length === 2){
+    return []
+  } else if (array.length % 2 === 0) {
+    return [array[middleIndex - 1], array[middleIndex]]
+  } else {
+    return [array[middleIndex]];
+  }
+};
+
+module.exports = middle;
